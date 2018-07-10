30ddfe78bfa1253ace5d04a202a97ec58205fb62
github username <github email address>
2018-07-10 17:01:11 -0400

    Teams Controller

--- SuperHeroDraft/app/controllers/api/v1/teams_controller.rb
+++ SuperHeroDraft/app/controllers/api/v1/teams_controller.rb
@@ -1,18 +1,33 @@
 class Api::V1::TeamsController < ApplicationController
 
   def index
-    @hero = Hero.all 
-    render json: @hero
-  end
-
-  def create
-    # team = Team.new(params[team])
-  
+    @teams = Team.all 
+    render json: @teams
   end
 
   def team_id
     request.headers['Authorization']
   end
+
+  def create
+
+    byebug
+
+    # team = Team.find_by(teamName: params[:teamName])
+    # if team && team.authenticate(params[:password])
+    #   render json: {teamName: team.teamName, id: team.id}, status: 200
+    # else
+    #   render json: {error: 'Username or Password Invalid'}, status: 401
+    # end
+  end
+
+  # def show
+  #   if logged_in
+  #     render json: {teamName: current_team.teamName, id: current_team.id}, status: 200
+  #   else
+  #     render json: {error: 'Token Invalid'}, status: 401
+  #   end
+  # end
 
 
 
